d43c3ec1f5b64d4fa0099a48bd91b9e69ebfa487
Alex Craig <acraig780@gmail.com>
2015-03-24 22:13:12 -0400

    initial project setup

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        sourceMap: false
+      },
+      build: {
+        files: {
+          'dist/jquery.directional-hover.min.js': ['src/jquery.directional-hover.js'],
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
